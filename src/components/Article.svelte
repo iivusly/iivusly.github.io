07c2a90724754bdc59a7d5c003ce80a4f12b6125
iivusly <52052910+iivusly@users.noreply.github.com>
2023-05-03 00:46:50 -0700

    fix article
    Signed-off-by: iivusly <52052910+iivusly@users.noreply.github.com>

--- src/components/Article.svelte
+++ src/components/Article.svelte
@@ -1,17 +1,10 @@
-<script lang="ts">
-  export let block = false
-</script>
-
 <article>
-  <slot />
+    <slot/>
 </article>
 
 <style lang="scss">
   article {
     display: block;
-    padding: 1rem;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-    margin-bottom: 1rem;
+    width: 100%;
   }
 </style>
