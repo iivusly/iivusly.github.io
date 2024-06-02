a33019874f0df689d52b85f2a59bb2547926550b
iivusly <iivusly@icloud.com>
2024-06-02 02:44:00 -0700

    add export to app router
    Signed-off-by: iivusly <iivusly@icloud.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    /**
+   * Enable static exports for the App Router.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: "export",
+};
 
 export default nextConfig;
