0a556dc3a7fde151c66f7ce28991fe5104c681b4
iivusly <iivusly@icloud.com>
2024-06-16 23:27:31 -0700

    fix eslint

--- eslint.config.mjs
+++
@@ -1,7 +0,0 @@
-const nextCoreWebVitals = require('next/core-web-vitals')
-const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended')
-
-module.exports = [
-    nextCoreWebVitals,
-    eslintPluginPrettierRecommended
-]
