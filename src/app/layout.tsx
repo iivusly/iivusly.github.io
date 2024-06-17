d96610f785dd8be97ff087f3a2ce8eb1eba84552
iivusly <iivusly@icloud.com>
2024-06-16 23:10:44 -0700

    remove unneeded styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import 'github-markdown-css';
 import StyledComponentsRegistry from "@lib/registry";
 
 const inter = Inter({ subsets: ["latin"] });
