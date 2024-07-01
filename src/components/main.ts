5a0df88f27d0ef61eccbb7b8f0f4625acd58af3b
iivusly <iivusly@icloud.com>
2024-07-01 14:54:29 -0700

    update css for mobile

--- src/components/main.ts
+++ src/components/main.ts
@@ -6,12 +6,14 @@ export const Breakpoint = 800
 
 const Main = styled.main`
 	display: flex;
-	justify-content: center;
+	justify-content: space-between;
 	align-items: center;
+	align-self: center;
 	flex-direction: column;
 	border-radius: 1rem;
 	margin: 2.5% 10%;
 	padding: 1rem;
+	word-wrap: break-word;
 
 	background: rgba(0, 0, 0, 0.5);
 	box-shadow: 0 8px 32px 0 rgba(255, 255, 255, 0.37);
@@ -23,7 +25,14 @@ const Main = styled.main`
 		top: 0;
 		left: 0;
 		width: 100vw;
+		padding: 0.5rem;
+		box-shadow: none;
 		border-radius: 0;
+		overflow-x: hidden;
+
+		* > * {
+			transform: scale(0.9);
+		}
 	}
 `
 
