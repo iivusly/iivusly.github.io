2390e9ef4fd6e57278874d59865c32b312634f29
iivusly <52052910+iivusly@users.noreply.github.com>
2026-02-18 23:03:10 -0800

    feat: more information about me yay!!!!

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,34 @@
+import Link from "next/link";
+
 export default function AboutPage() {
   return (
     <>
       <h1>About Me</h1>
-      <p>Nothing here, yet!</p>
+      <p>
+        Hi, I am Niko (known as iivusly, lem0n, or n1kital online). I am a
+        language enthusiast (natural and formal). I speak English formally, but
+        also know French and Spanish (via proxy). My goal is to learn at least
+        one language from each of the families to fluency! I have a half baked
+        understanding of gramatial structure and basic grammar of German,
+        Russian, Japanese, and Chinese. I love to code, listen to music, and
+        play video games in my free time. I have been an avid programmer since
+        2017 and since then havent gotten bored.
+      </p>
+      <h2>Contact Me</h2>
+      <p>No seriously, please do I am always looking for more friends!</p>
+      <ul>
+        <li>
+          <Link href="https://discord.com/users/1041597787407663134">
+            Discord
+          </Link>
+        </li>
+        <li>
+          <Link href="https://t.me/lemonyseal">Telegram</Link>
+        </li>
+        <li>
+          <Link href="https://matrix.to/#/@ponzu_sause:matrix.org">Matrix</Link>
+        </li>
+      </ul>
     </>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,8 @@ export default async function HomePage() {
       <p>
         This is the home page to my website. It is not done yet, so stick
         around! If you're looking for some information about me, check out my
-        about page <Link href="/about">here!</Link>
+        about page <Link href="/about">here!</Link> Sorry if some of the English
+        is poor, this is only the first iteration.
       </p>
     </>
   );
